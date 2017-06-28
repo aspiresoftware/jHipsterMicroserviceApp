66a301648ef5bdbba44391b48a71764613c62a5a
pratikfromaspire <pratik.patel@aspiresoftware.in>
2017-06-28 14:03:52 +0530

    initial commit

---
+++ src/main/java/com/aspire/microservice/domain/Customer.java
@@ -0,0 +1,75 @@
+package com.aspire.microservice.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Customer.
+ */
+@Entity
+@Table(name = "customer")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Customer firstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Customer customer = (Customer) o;
+        if (customer.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), customer.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+            "id=" + getId() +
+            ", firstName='" + getFirstName() + "'" +
+            "}";
+    }
+}
---
+++ src/test/java/com/aspire/microservice/web/rest/CustomerResourceIntTest.java
@@ -0,0 +1,235 @@
+package com.aspire.microservice.web.rest;
+
+import com.aspire.microservice.JHipsterMicroserviceApp;
+
+import com.aspire.microservice.domain.Customer;
+import com.aspire.microservice.repository.CustomerRepository;
+import com.aspire.microservice.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the CustomerResource REST controller.
+ *
+ * @see CustomerResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JHipsterMicroserviceApp.class)
+public class CustomerResourceIntTest {
+
+    private static final String DEFAULT_FIRST_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_FIRST_NAME = "BBBBBBBBBB";
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    @Autowired
+    private EntityManager em;
+
+    private MockMvc restCustomerMockMvc;
+
+    private Customer customer;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        CustomerResource customerResource = new CustomerResource(customerRepository);
+        this.restCustomerMockMvc = MockMvcBuilders.standaloneSetup(customerResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Customer createEntity(EntityManager em) {
+        Customer customer = new Customer()
+            .firstName(DEFAULT_FIRST_NAME);
+        return customer;
+    }
+
+    @Before
+    public void initTest() {
+        customer = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createCustomer() throws Exception {
+        int databaseSizeBeforeCreate = customerRepository.findAll().size();
+
+        // Create the Customer
+        restCustomerMockMvc.perform(post("/api/customers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(customer)))
+            .andExpect(status().isCreated());
+
+        // Validate the Customer in the database
+        List<Customer> customerList = customerRepository.findAll();
+        assertThat(customerList).hasSize(databaseSizeBeforeCreate + 1);
+        Customer testCustomer = customerList.get(customerList.size() - 1);
+        assertThat(testCustomer.getFirstName()).isEqualTo(DEFAULT_FIRST_NAME);
+    }
+
+    @Test
+    @Transactional
+    public void createCustomerWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = customerRepository.findAll().size();
+
+        // Create the Customer with an existing ID
+        customer.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restCustomerMockMvc.perform(post("/api/customers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(customer)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Alice in the database
+        List<Customer> customerList = customerRepository.findAll();
+        assertThat(customerList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void getAllCustomers() throws Exception {
+        // Initialize the database
+        customerRepository.saveAndFlush(customer);
+
+        // Get all the customerList
+        restCustomerMockMvc.perform(get("/api/customers?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(customer.getId().intValue())))
+            .andExpect(jsonPath("$.[*].firstName").value(hasItem(DEFAULT_FIRST_NAME.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void getCustomer() throws Exception {
+        // Initialize the database
+        customerRepository.saveAndFlush(customer);
+
+        // Get the customer
+        restCustomerMockMvc.perform(get("/api/customers/{id}", customer.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(customer.getId().intValue()))
+            .andExpect(jsonPath("$.firstName").value(DEFAULT_FIRST_NAME.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingCustomer() throws Exception {
+        // Get the customer
+        restCustomerMockMvc.perform(get("/api/customers/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateCustomer() throws Exception {
+        // Initialize the database
+        customerRepository.saveAndFlush(customer);
+        int databaseSizeBeforeUpdate = customerRepository.findAll().size();
+
+        // Update the customer
+        Customer updatedCustomer = customerRepository.findOne(customer.getId());
+        updatedCustomer
+            .firstName(UPDATED_FIRST_NAME);
+
+        restCustomerMockMvc.perform(put("/api/customers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedCustomer)))
+            .andExpect(status().isOk());
+
+        // Validate the Customer in the database
+        List<Customer> customerList = customerRepository.findAll();
+        assertThat(customerList).hasSize(databaseSizeBeforeUpdate);
+        Customer testCustomer = customerList.get(customerList.size() - 1);
+        assertThat(testCustomer.getFirstName()).isEqualTo(UPDATED_FIRST_NAME);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingCustomer() throws Exception {
+        int databaseSizeBeforeUpdate = customerRepository.findAll().size();
+
+        // Create the Customer
+
+        // If the entity doesn't have an ID, it will be created instead of just being updated
+        restCustomerMockMvc.perform(put("/api/customers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(customer)))
+            .andExpect(status().isCreated());
+
+        // Validate the Customer in the database
+        List<Customer> customerList = customerRepository.findAll();
+        assertThat(customerList).hasSize(databaseSizeBeforeUpdate + 1);
+    }
+
+    @Test
+    @Transactional
+    public void deleteCustomer() throws Exception {
+        // Initialize the database
+        customerRepository.saveAndFlush(customer);
+        int databaseSizeBeforeDelete = customerRepository.findAll().size();
+
+        // Get the customer
+        restCustomerMockMvc.perform(delete("/api/customers/{id}", customer.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<Customer> customerList = customerRepository.findAll();
+        assertThat(customerList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Customer.class);
+        Customer customer1 = new Customer();
+        customer1.setId(1L);
+        Customer customer2 = new Customer();
+        customer2.setId(customer1.getId());
+        assertThat(customer1).isEqualTo(customer2);
+        customer2.setId(2L);
+        assertThat(customer1).isNotEqualTo(customer2);
+        customer1.setId(null);
+        assertThat(customer1).isNotEqualTo(customer2);
+    }
+}
